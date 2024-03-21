7223f81b4503690189e196c541bb5f66bb3bb00d
daveee1 <76774480+daveee1@users.noreply.github.com>
2024-03-21 12:05:48 +0100

    Update goodNodes.py

--- goodNodes.py
+++ goodNodes.py
@@ -1,32 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        count=1#consider root
-        lista=[]
-        lista.append(root.val)
-        ans=self.ff(root,lista,count)
-        return ans
-        
-        
-    def ff(self,node, lista , count):
-        if not node:
-            return 0
-        big=max(lista)
-        lista.append(node.val)
-        r1=self.ff(node.left,lista,count)
-        r2=self.ff(node.right,lista,count)
-        count=r2+r1
-        if node.val>=big:
-            count+=1
-        lista.remove(node.val)
-        return count
-        
-
-        
+    # Definition for a binary tree node.
+    # class TreeNode:
+    #     def __init__(self, val=0, left=None, right=None):
+    #         self.val = val
+    #         self.left = left
+    #         self.right = right
+    class Solution:
+        def goodNodes(self, root: TreeNode) -> int:
+            if not root:
+                return 0
+            count=1#consider root
+            ans=self.ff(root,root.val)
+            return ans        
+        def ff(self,node, big):
+            if not node:
+                return 0
+            big=max(node.val,big)
+            r1=self.ff(node.left,big)
+            r2=self.ff(node.right,big)
+            sum=r2+r1
+            if node.val>=big:
+                sum+=1
+            return sum
