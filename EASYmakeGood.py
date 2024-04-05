baeb7a068de1012f318aebe894c98b18d564d932
daveee1 <76774480+daveee1@users.noreply.github.com>
2024-04-05 18:24:54 +0200

    Update EASYmakeGood.py

--- EASYmakeGood.py
+++ EASYmakeGood.py
@@ -11,6 +11,22 @@
 #Notice that an empty string is also good
 
 
+
+
+#chatgpt
+    #class Solution:
+#    def makeGood(self, s: str) -> str:
+  #      stack = []
+        
+      #  for char in s:
+        #    if stack and abs(ord(stack[-1]) - ord(char)) == 32:
+         #       stack.pop()
+          #  else:
+           #     stack.append(char)
+        
+       # return ''.join(stack)
+
+
 class Solution:
     def makeGood(self, s: str) -> str:
         n=len(s)
